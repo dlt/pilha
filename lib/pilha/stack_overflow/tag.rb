8e165d66975d36449a630a105ff42423f0599e0c
dalto curvelano jr <daltojr@monster.localdomain>
2010-06-03 11:15:23 -0300

    added support for /users/:id/tags

--- lib/pilha/stack_overflow/tag.rb
+++ lib/pilha/stack_overflow/tag.rb
@@ -3,13 +3,16 @@ module StackExchange
     class Tag < Base
       extend Forwardable
 
-      def_delegators :@struct, :name, :count
+      def_delegators :@struct, :name, :count, :user_id
 
       class << self
         def all(options = {})
           request('/tags', nil, options)
         end
 
+        def find_by_user_id(id, options = {})
+          request('/users/:id/tags', id, options)
+        end
 
         def parse(response)
           parse_with_class(response, 'tags', Tag)
--- spec/pilha/stack_overflow/tag_spec.rb
+++ spec/pilha/stack_overflow/tag_spec.rb
@@ -15,4 +15,22 @@ describe StackExchange::StackOverflow::Tag do
     tag.count.should == 84875
   end
 
+  it 'should find all tags used by an user identified by id' do
+    response = StackOverflow::Tag.find_by_user_id(549)
+    response.total.should == 151
+    response.page.should == 1
+
+
+    tag = response.tags.first
+    tag.name.should == ".net"
+    tag.count.should == 25
+    tag.user_id.should == 549
+
+    response.tags.should have(response.pagesize).tags
+
+    response.tags.each do |tag|
+      tag.user_id.should == 549
+    end
+  end
+
 end
